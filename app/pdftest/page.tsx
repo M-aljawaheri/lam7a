bbf9f116bbc288905aebb71918125181d7ebf303
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-10-21 23:18:39 +0300

    pdf test

---
+++ app/pdftest/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import PdfViewer from "../components/PdfViewer";
+
+const Page = () => {
+  return (
+    <>
+      <PdfViewer fileUrl={"./assets/lam7a_issue7_September2023.pdf"} />
+    </>
+  );
+};
+export default Page;
