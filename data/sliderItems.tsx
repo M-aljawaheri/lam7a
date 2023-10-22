2bbaf170ed3c70b243090fdd65b90467bf6e4cc5
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-10-22 10:39:27 +0300

    optimized pdf

--- data/sliderItems.tsx
+++ data/sliderItems.tsx
@@ -6,18 +6,18 @@ type SliderImage = {
 export const sliderItems: SliderImage[] = [
     {
         image: "/journals/img132.jpg",
-        link: "lam7a_issue7_September2023.pdf",
+        link: "september-opt.pdf",
     },
     {
         image: "/journals/img133.jpg",
-        link: "lam7a_issue7_September2023.pdf",
+        link: "september-opt.pdf",
     },
     {
         image: "/journals/img134.jpg",
-        link: "lam7a_issue7_September2023.pdf",
+        link: "september-opt.pdf",
     },
     {
         image: "/journals/img135.jpg",
-        link: "lam7a_issue7_September2023.pdf",
+        link: "september-opt.pdf",
     },
 ];
