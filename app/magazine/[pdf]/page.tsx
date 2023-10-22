6d7d35fea927a03909bac55923deaecf5734cd5c
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-10-22 10:55:02 +0300

    add slider to the pdf viewer

--- app/magazine/[pdf]/page.tsx
+++ app/magazine/[pdf]/page.tsx
@@ -1,12 +1,15 @@
-// export default function Page({ params }: { params: { pdf: string } }) {
-//     return <div>My Post: {params.pdf}</div>
-//   }
 "use client";
+import Slider from "@/app/components/slider";
 import PdfViewer from "../../components/PdfViewer";
 const Page = ({ params }: { params: { pdf: string } }) => {
   return (
     <>
       <PdfViewer fileUrl={`/assets/${params.pdf}`} />
+      <div className="flex flex-col h-max py-1 lg:pb-3.5 lg:pt-8 lg:px-20 bg-customOrange">
+        <div className='mt-10'>
+          <Slider/>
+        </div>
+      </div>
     </>
   );
 };
