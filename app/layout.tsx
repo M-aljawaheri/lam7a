3c0dedc9e675e2f473e1078baa626d944d3de415
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-11-22 10:51:11 +0300

    updated footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 "use client";
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -12,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
