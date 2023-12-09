4e9b4c359308d6ffb7d0da23f58a824c9ab58a5f
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-12-09 16:17:36 +0300

    october issue

--- data/sliderItems.tsx
+++ data/sliderItems.tsx
@@ -5,32 +5,36 @@ type SliderImage = {
 
 export const sliderItems: SliderImage[] = [
     {
+        image: "/journals/Oct23.png",
+        link: "Oct23.pdf",
+    },
+    {
         image: "/journals/Sep23.jpg",
         link: "Sep23.pdf",
     },
     {
-        image: "/journals/Aug22.jpg",
-        link: "Aug22.pdf",
-    },
-    {
-        image: "/journals/Sep22.jpg",
-        link: "Sep22.pdf",
-    },
-    {
-        image: "/journals/Oct22.jpg",
-        link: "Oct22.pdf",
-    },
-    {
-        image: "/journals/Jan23.jpg",
-        link: "Jan23.pdf",
+        image: "/journals/Mar23.jpg",
+        link: "Mar23.pdf",
     },
     {
         image: "/journals/Feb23.jpg",
         link: "Feb23.pdf",
     },
     {
-        image: "/journals/Mar23.jpg",
-        link: "Mar23.pdf",
+        image: "/journals/Jan23.jpg",
+        link: "Jan23.pdf",
     },
-    
+    {
+        image: "/journals/Oct22.jpg",
+        link: "Oct22.pdf",
+    },
+    {
+        image: "/journals/Sep22.jpg",
+        link: "Sep22.pdf",
+    },
+    {
+        image: "/journals/Aug22.jpg",
+        link: "Aug22.pdf",
+    },
+
 ];
