dd49173f243e5df41fb40b82da496051c997053b
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2024-04-01 16:57:51 +0300

    jan24

--- data/sliderItems.tsx
+++ data/sliderItems.tsx
@@ -1,40 +1,43 @@
 type SliderImage = {
-    image: string;
-    link: string;
+  image: string;
+  link: string;
 };
 
 export const sliderItems: SliderImage[] = [
-    {
-        image: "/journals/Oct23.png",
-        link: "Oct23.pdf",
-    },
-    {
-        image: "/journals/Sep23.jpg",
-        link: "Sep23.pdf",
-    },
-    {
-        image: "/journals/Mar23.jpg",
-        link: "Mar23.pdf",
-    },
-    {
-        image: "/journals/Feb23.jpg",
-        link: "Feb23.pdf",
-    },
-    {
-        image: "/journals/Jan23.jpg",
-        link: "Jan23.pdf",
-    },
-    {
-        image: "/journals/Oct22.jpg",
-        link: "Oct22.pdf",
-    },
-    {
-        image: "/journals/Sep22.jpg",
-        link: "Sep22.pdf",
-    },
-    {
-        image: "/journals/Aug22.jpg",
-        link: "Aug22.pdf",
-    },
-
+  {
+    image: "/journals/Jan24.jpg",
+    link: "Jan24.pdf",
+  },
+  {
+    image: "/journals/Oct23.png",
+    link: "Oct23.pdf",
+  },
+  {
+    image: "/journals/Sep23.jpg",
+    link: "Sep23.pdf",
+  },
+  {
+    image: "/journals/Mar23.jpg",
+    link: "Mar23.pdf",
+  },
+  {
+    image: "/journals/Feb23.jpg",
+    link: "Feb23.pdf",
+  },
+  {
+    image: "/journals/Jan23.jpg",
+    link: "Jan23.pdf",
+  },
+  {
+    image: "/journals/Oct22.jpg",
+    link: "Oct22.pdf",
+  },
+  {
+    image: "/journals/Sep22.jpg",
+    link: "Sep22.pdf",
+  },
+  {
+    image: "/journals/Aug22.jpg",
+    link: "Aug22.pdf",
+  },
 ];
